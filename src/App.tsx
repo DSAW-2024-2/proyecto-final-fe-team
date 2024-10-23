450a3f8e2860fd87524b0ccd21468dbdce1d9ea1
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-10-23 17:34:05 -0500

    Adding a starting page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignInPage from './components/SignInPage.tsx';
 import SignUpPage from './components/SignUpPage.tsx';
+import LoadingFirstPage from './LoadingFirstPage.tsx';
 
 
 const App: React.FC = () => {
 	return (
 	  <Router>
 		<Routes>
+		  <Route path="/" element={<LoadingFirstPage />} />
 		  <Route path="/register" element={<SignUpPage />} />
 		  <Route path="/login" element={<SignInPage />} />
 		</Routes>
---
+++ src/LoadingFirstPage.tsx
@@ -0,0 +1,10 @@
+function LoadingFirstPage() {
+  return (
+    <div className="bg-blue w-full h-[100vh] flex flex-col justify-center items-center text-white">
+      <h1 className=" text-[40px]"><span className="font-bold">Uni</span>Lift</h1>
+      <h3 className="tracking-[0.5em] text-[10px]">YOUR EASY LIFT</h3>
+    </div>
+  );
+}
+
+export default LoadingFirstPage;
