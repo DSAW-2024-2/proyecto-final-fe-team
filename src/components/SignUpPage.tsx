af0e5d31b77806f56b5c7f3bb295e898059533d0
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-10-23 08:15:20 -0500

    Changes in sign up page style

--- src/components/SignUpPage.tsx
+++ src/components/SignUpPage.tsx
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 interface FormData{
-    IdUni: number,
+    IdUni: string,
     email: string,
-    phone: number,
+    phone: string,
     firstName: string,
     lastName: string,
     password: string,
@@ -19,9 +19,9 @@ interface ApiResponse {
 function SignUpPage() {
 
     const [formData, setFormData] = useState<FormData>({
-        IdUni: 0,
+        IdUni: '',
         email: '',
-        phone: 0,
+        phone: '',
         firstName: '',
         lastName: '',
         password: '',
@@ -49,108 +49,116 @@ function SignUpPage() {
 
    
 return (
-    <div className="flex justify-center">
-    <div className="w-full max-w-md px-6 py-12">
-        <div className="text-center">
-            <FontAwesomeIcon icon={faUser} style={{ color: '#152E52' }} className="m-2 text-3xl" />
-            <h2 className="text-2xl font-bold m-4 text-blue">Registrarse</h2>
-            <p className="text-blue m-4">Ingresa tus datos para disfrutar de todos nuestros servicios</p>
+    <div className="flex">
+
+        <div className="flex lg:w-1/2 md:justify-center  md:bg-blue">
+
         </div>
 
-        <form onSubmit={handleSubmit} className="space-y-6">
-            <div>
-                <label htmlFor="IdUni" className="block font-medium text-blue">ID universitario</label>
-                <input 
-                    id="IdUni" 
-                    name="IdUni" 
-                    type="number" 
-                    required
-                    value={formData.IdUni}
-                    onChange={handleChange}
-                    className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
-                />
+        <div className="hidden w-full lg:w-1/2 lg:flex lg:flex-col lg:items-center px-6 py-16">
+            <div className="w-full max-w-sm px-6 py-12">
+
+            <div className="text-center">
+                <FontAwesomeIcon icon={faUser} style={{ color: '#152E52' }} className="m-2 text-3xl" />
+                <h2 className="text-2xl font-bold m-4 text-blue">Registrarse</h2>
+                <p className="text-blue m-4">Ingresa tus datos para disfrutar de todos nuestros servicios</p>
             </div>
 
-            <div>
-                <label htmlFor="email" className="block font-medium text-blue">Correo corporativo</label>
-                <input 
-                    id="email" 
-                    name="email" 
-                    type="email" 
-                    required
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
-                />
+            <form onSubmit={handleSubmit} className="space-y-6">
+                <div>
+                    <label htmlFor="IdUni" className="block font-medium text-blue">ID universitario</label>
+                    <input 
+                        id="IdUni" 
+                        name="IdUni" 
+                        type="number" 
+                        required
+                        value={formData.IdUni}
+                        onChange={handleChange}
+                        className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="email" className="block font-medium text-blue">Correo corporativo</label>
+                    <input 
+                        id="email" 
+                        name="email" 
+                        type="email" 
+                        required
+                        value={formData.email}
+                        onChange={handleChange}
+                        className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="phone" className="block font-medium text-blue">Número de contacto</label>
+                    <input 
+                        id="phone" 
+                        name="phone" 
+                        type="number" 
+                        required
+                        value={formData.phone}
+                        onChange={handleChange}
+                        className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="firstName" className="block font-medium text-blue">Nombre</label>
+                    <input 
+                        id="firstName" 
+                        name="firstName" 
+                        type="text" 
+                        required
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="lastName" className="block font-medium text-blue">Apellido</label>
+                    <input 
+                        id="lastName" 
+                        name="lastName" 
+                        type="text" 
+                        required
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
+                    />
+                </div>
+
+
+                <div>
+                    <label htmlFor="password" className="block font-medium text-blue">Contraseña</label>
+                    <input 
+                        id="password" 
+                        name="password" 
+                        type="password" 
+                        required
+                        value={formData.password}
+                        onChange={handleChange}
+                        className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
+                    />
+                </div>
+
+                <button 
+                    type="submit"
+                    disabled={isLoading}
+                    className={`w-full h-11 bg-green text-white rounded-3xl font-semibold shadow-sm hover:bg-blue ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                >
+                    {isLoading ? 'Creando cuenta...' : 'Crear Cuenta'}
+                </button>
+            </form>
+
+            <p className="mt-6 text-center text-gray-500">
+                ¿Ya tienes una cuenta?
+                <a href="#" className="text-green hover:text-blue ml-1">Inicia sesión aquí</a>
+            </p>
             </div>
-
-            <div>
-                <label htmlFor="phone" className="block font-medium text-blue">Número de contacto</label>
-                <input 
-                    id="phone" 
-                    name="phone" 
-                    type="number" 
-                    required
-                    value={formData.phone}
-                    onChange={handleChange}
-                    className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
-                />
-            </div>
-
-            <div>
-                <label htmlFor="firstName" className="block font-medium text-blue">Nombre</label>
-                <input 
-                    id="firstName" 
-                    name="firstName" 
-                    type="text" 
-                    required
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
-                />
-            </div>
-
-            <div>
-                <label htmlFor="lastName" className="block font-medium text-blue">Apellido</label>
-                <input 
-                    id="lastName" 
-                    name="lastName" 
-                    type="text" 
-                    required
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
-                />
-            </div>
-
-
-            <div>
-                <label htmlFor="password" className="block font-medium text-blue">Contraseña</label>
-                <input 
-                    id="password" 
-                    name="password" 
-                    type="password" 
-                    required
-                    value={formData.password}
-                    onChange={handleChange}
-                    className="mt-2 w-full rounded-3xl border py-2 px-3 shadow-sm focus:ring-2 focus:ring-green focus:outline-none"  
-                />
-            </div>
-
-            <button 
-                type="submit"
-                disabled={isLoading}
-                className={`w-full h-11 bg-green text-white rounded-3xl font-semibold shadow-sm hover:bg-blue ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
-            >
-                {isLoading ? 'Creando cuenta...' : 'Crear Cuenta'}
-            </button>
-        </form>
-
-        <p className="mt-6 text-center text-gray-500">
-            ¿Ya tienes una cuenta?
-            <a href="#" className="text-green hover:text-blue ml-1">Inicia sesión aquí</a>
-        </p>
-    </div>
+        </div>
 </div>
     );
 }
