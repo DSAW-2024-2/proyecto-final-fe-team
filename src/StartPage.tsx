61c7834ff51610ad8960921bb9b7f3af1d298e39
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-10-24 07:20:27 -0500

    Adding navegation option into the start page

--- src/StartPage.tsx
+++ src/StartPage.tsx
@@ -1,4 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function StartPage() {
+
+    const navigate = useNavigate();
+
+    const handleSignIn = () => {
+        navigate('/login');
+    }
+
+    const handleSignUp = () => {
+        navigate('/register');
+    }
+
     return (
         <div className="flex">
             
@@ -13,16 +26,16 @@ function StartPage() {
                 <h3 className="text-[11px] tracking-[0.5em] font-semibold text-gray-500">YOUR EASY LIFT</h3>
             </section>
             
-            <section className="m-6">
-                <h1 className="text-[30px] text-blue my-10 text-center">Te damos la bienvenida a <span className="font-bold">UniLift</span></h1>
+            <section className="m-10 flex flex-col justify-center">
+                <h1 className="text-[30px] text-blue my-10 -mx-4 text-center">Te damos la bienvenida a <span className="font-bold">UniLift</span></h1>
                 <button type="submit"
-                            className="w-full h-11 bg-green text-white rounded-3xl font-semibold shadow-sm hover:bg-blue mt-10">
+                            className="w-full h-11 bg-green text-white rounded-3xl font-semibold shadow-sm hover:bg-blue mt-10" onClick={handleSignIn}>
                             Iniciar Sesión
             </button>
 
             <p className="mt-6 text-center text-gray-500">
                         ¿No tienes una cuenta?
-                        <a href="#" className="text-green hover:text-blue ml-1">Regístrate aquí</a>
+                        <a href="" className="text-green hover:text-blue ml-1" onClick={handleSignUp}>Regístrate aquí</a>
             </p>
             </section>
             </div>
